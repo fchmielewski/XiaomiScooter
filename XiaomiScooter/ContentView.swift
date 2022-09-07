cbd68af5a94ab596f2a6f5c605551578c370ef44
fchmielewski <112649524+fchmielewski@users.noreply.github.com>
2022-09-07 12:12:45 +0200

    Add files via upload

---
+++ XiaomiScooter/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  ScooterApp
+//
+//  Created by Filip Chmielewski on 07/09/2022.
+//
+import SwiftUI
+
+struct ContentView: View {
+    var isLocked: Bool
+
+    var body: some View {
+        VStack {
+            Label("100%, DTE 45km", systemImage: "battery.100")
+            Image("mipro2")
+                .resizable()
+                .frame(width: 320, height: 320)
+            Button(isLocked ? "UNLOCK" : "LOCK") {
+                print("Lock button tapped!")
+            }
+            .buttonStyle(.borderedProminent)
+        }
+        .navigationTitle("Mi Scooter Pro 2")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ContentView(isLocked: false)
+        }
+    }
+}
---
+++ XiaomiScooter/XiaomiScooterApp.swift
@@ -0,0 +1,17 @@
+//
+//  XiaomiScooterApp.swift
+//  XiaomiScooter
+//
+//  Created by Filip Chmielewski on 07/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct XiaomiScooterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(isLocked: false)
+        }
+    }
+}
